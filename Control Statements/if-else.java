e8ae422ccc8bdff70aa688d41def43cd6ba6e4cd
breakingozzy <breakingozymandias@gmail.com>
2026-08-31 19:46:35 +0530

    Create if-else.java

---
+++ Control Statements/if-else.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Not Adult");
+        }
+    }
+}
