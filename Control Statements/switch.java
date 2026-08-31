41e09dc67598e76de8f9c4d4660b7b18e5c530cb
breakingozzy <breakingozymandias@gmail.com>
2026-08-31 19:48:37 +0530

    Create switch.java

---
+++ Control Statements/switch.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int day = 1;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            default:
+                System.out.println("Other day");
+        }
+    }
+}
